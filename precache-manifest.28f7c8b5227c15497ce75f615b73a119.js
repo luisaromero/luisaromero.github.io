a75d91d5ebbd2dfb15485343a59499d4f1255a11
luisaromero <luisa.marcela.romero@gmail.com>
2019-08-02 21:09:13 -0400

    Updates

---
+++ precache-manifest.28f7c8b5227c15497ce75f615b73a119.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03afaa84631a1362a0ff0a09cb772c5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0a601786317af47abcf",
+    "url": "/static/css/main.74628f12.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c0a601786317af47abcf",
+    "url": "/static/js/main.1a20b4ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
