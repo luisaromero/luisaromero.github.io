4b7136b5b8df631901f996f72ba2c797d9b4b2db
luisaromero <luisa.marcela.romero@gmail.com>
2019-09-02 14:24:45 -0400

    agregando detalles al css

--- src/cards.jsx
+++ src/cards.jsx
@@ -9,7 +9,7 @@ class Cards extends Component{
  <div className="container"> 
  <div className="row">
   <div className="col-12 col-md-3 img-news">
-      <img className="img-fluid" src="https://cdn.about.me/s3/h/z/social.c1bd328b.png" alt=""/>
+      <img className="img-fluid" src="https://cdn.about.me/s3/h/z/social.c1bd328b.png" alt="proyecto"/>
   </div>
   <div className="col-12 col-md-3 img-news">
       <img className="img-fluid" src="https://1kabswnt2ua3ivl0cuqv2f17-wpengine.netdna-ssl.com/wp-content/uploads/2014/06/Skills.jpg" alt=""/>
