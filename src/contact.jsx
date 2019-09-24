634feaa6297eb1844cd0d428d7f8aa6fa002a93e
luisaromero <luisa.marcela.romero@gmail.com>
2019-09-24 17:51:35 +0100

    add enlaces a contacto

--- src/contact.jsx
+++ src/contact.jsx
@@ -11,9 +11,9 @@ class Contact extends Component{
             <p>_______________</p>
             <h1>Contacto</h1>
         </div>
-        <img src="https://www.freeiconspng.com/uploads/github-logo-icon-30.png"></img>
-        <img src="http://www.pngall.com/wp-content/uploads/2016/07/Linkedin-Free-Download-PNG.png"></img>
-       <img src="http://asociacionfilio.es/wp-content/uploads/2019/01/envelope_mail_icon-icons.com_66119.png"></img>
+       <a target="_blank" href="https://github.com/luisaromero"><img src="https://www.freeiconspng.com/uploads/github-logo-icon-30.png"></img></a>
+       <a  target="_blank" href="https://www.linkedin.com/in/luisa-romero-cotrena"> <img src="http://www.pngall.com/wp-content/uploads/2016/07/Linkedin-Free-Download-PNG.png"></img></a>
+      <a  target="_blank" href=""> <img src="http://asociacionfilio.es/wp-content/uploads/2019/01/envelope_mail_icon-icons.com_66119.png"></img></a>
     </div>
 </React.Fragment>
 )
