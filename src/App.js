9fe8e5f63859409f40af4927eb43de3043b14c6a
luisaromero <luisa.marcela.romero@gmail.com>
2020-03-07 15:25:50 -0300

    navbar sticky

--- src/App.js
+++ src/App.js
@@ -6,7 +6,22 @@ import '././App.css';
 class App extends React.Component{
   render(){
       return(
-      <div>hola</div>
+     
+        <React.Fragment>
+          <nav className="navbar navbar-expand-sm navbar-dark bg-primary sticky-top">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+             <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+             <div className="navbar-nav">
+          <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+      <a className="nav-item nav-link" href="#">Features</a>
+      <a className="nav-item nav-link" href="#">Pricing</a>
+      </div>
+      </div>
+          </nav>
+      
+          </React.Fragment>
       )}
 }
 
