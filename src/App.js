49bfacb4fc00b70fdcff66b70a03a75d8d3bf9ef
luisaromero <luisa.marcela.romero@gmail.com>
2020-03-07 13:38:20 -0300

    eliminando archivos y haciendo portafolionuevo

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
 import React from 'react';
 import '././App.css';
-import Footer from '././footer';
-import Cards from '././cards';
-import Slider from '././Slider';
-import Contact from '././contact';
+
 
 
 class App extends React.Component{
   render(){
       return(
-        <React.Fragment>     
-          <div className="containerCover">
-    <div className="overlay">
-      <nav className="navbar navbar-expand-lg ">
-  <div className="collapse navbar-collapse" id="navbarNav">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href="#about-me">Sobre mi </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#projects">Proyectos</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#contact">Contacto</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-    <div className="hola text-center">
-      <p id="pp">___</p>
-    <h1>
-     Luisa Romero
-    </h1>
-    
-    <h2>Front-end Developer</h2>
-    </div>
-    </div>
-    </div>  
-    <div id="about-me"><Slider/></div>
-    
-    <div id="projects"><Cards/></div>
-    <div id="contact"><Contact/></div>
-    
-   <Footer/>   
-    </React.Fragment>
-  )}
+      <div>hola</div>
+      )}
 }
 
 export default App;
