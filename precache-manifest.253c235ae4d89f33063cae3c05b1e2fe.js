8995e11118d9863eedffdc7c12bd74f76d601a16
luisaromero <luisa.marcela.romero@gmail.com>
2020-04-17 18:35:42 -0400

    Updates

---
+++ precache-manifest.253c235ae4d89f33063cae3c05b1e2fe.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e81a9a4a3ddb88f125671bc471f5405",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3cee30a72a69b4abc7d",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "a6ab938f3dd790ef9ede",
+    "url": "/static/css/main.824905b4.chunk.css"
+  },
+  {
+    "revision": "b3cee30a72a69b4abc7d",
+    "url": "/static/js/2.cffc6ce9.chunk.js"
+  },
+  {
+    "revision": "6b74098ea759ae59479fe11b592e06c8",
+    "url": "/static/js/2.cffc6ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6ab938f3dd790ef9ede",
+    "url": "/static/js/main.0ee3e023.chunk.js"
+  },
+  {
+    "revision": "d875b6f2ec20d12f3323",
+    "url": "/static/js/runtime-main.6a1508d6.js"
+  },
+  {
+    "revision": "4588517576d0de1e8f30733593f436ea",
+    "url": "/static/media/bootstrap-brands.45885175.svg"
+  },
+  {
+    "revision": "ea7920ed5a5c695c768777d50d406ca9",
+    "url": "/static/media/css3-brands.ea7920ed.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a22c2dd6b9697d24357c7b3eb828ac2c",
+    "url": "/static/media/git-alt-brands.a22c2dd6.svg"
+  },
+  {
+    "revision": "99325d4efd59006419033a0523a5b0ba",
+    "url": "/static/media/html5-brands.99325d4e.svg"
+  },
+  {
+    "revision": "3094c4233431edb41ad689a35effadc8",
+    "url": "/static/media/js-brands.3094c423.svg"
+  },
+  {
+    "revision": "cdc525fef1db60b5ba5c2524d80a06ce",
+    "url": "/static/media/node-js-brands.cdc525fe.svg"
+  },
+  {
+    "revision": "d8cf77403db1c802576acc37627ff00b",
+    "url": "/static/media/react-brands.d8cf7740.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da6e0a364ffaae39a18e4c040bd3cedb.js"
+  "/precache-manifest.253c235ae4d89f33063cae3c05b1e2fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
